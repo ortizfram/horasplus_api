a5081bbb230ad232207c3c9cb5c602517912f320
Franco Ortiz <ortizfranco48@yahoo.com>
2024-09-23 13:41:59 -0300

    horas totales

--- model/User.js
+++ model/User.js
@@ -1,30 +1,30 @@
-const mongoose = require("mongoose");
+  const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema(
-  {
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    firstname: { type: String, required: false },
-    lastname: { type: String, required: false },
-    profile_pic: { type: String, required: false },
-    organization_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Organization",
-      required: false,
+  const userSchema = new mongoose.Schema(
+    {
+      email: { type: String, required: true },
+      password: { type: String, required: true },
+      firstname: { type: String, required: false },
+      lastname: { type: String, required: false },
+      profile_pic: { type: String, required: false },
+      organization_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Organization",
+        required: false,
+      },
+      role: {
+        type: [String],
+        enum: ["User", "Admin"],
+        default: ["User"],
+      },
+      hourly_fee: { type: Number, required: true, default: 0 },
+      declared_hours: { type: Number, required: true, default: 0 },
+      travel_cost: { type: Number, required: true, default: 0 },
     },
-    role: {
-      type: [String],
-      enum: ["User", "Admin"],
-      default: ["User"],
-    },
-    hourly_fee: { type: Number, required: true, default: 0 },
-    declared_hours: { type: Number, required: true, default: 0 },
-    travel_cost: { type: Number, required: true, default: 0 },
-  },
-  {
-    timestamps: true,
-  }
-);
+    {
+      timestamps: true,
+    }
+  );
 
-//Compile to form the model
-module.exports = mongoose.model("User", userSchema);
+  //Compile to form the model
+  module.exports = mongoose.model("User", userSchema);
