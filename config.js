715861cefe3aff6f790f88670b8bca6a73ad56f2
Franco Ortiz <ortizfranco48@yahoo.com>
2024-11-22 13:41:22 -0300

    config

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const isDev = process.env.NODE_ENV === "development";
 
 
-const ORIGIN_URL = !isDev ? process.env.ORIGIN_URL : "http://localhost:8081";
+const ORIGIN_URL = process.env.ORIGIN_URL //: "http://localhost:8081";
 const NODEMAILER_EMAIL = process.env.NODEMAILER_EMAIL;
 const NODEMAILER_PASS = process.env.NODEMAILER_PASS;
 const BRAND_EMAIL = process.env.BRAND_EMAIL;
-const MONGO_URI = !isDev ? process.env.MONGO_URI : "mongodb://localhost:27017/auth-api";
+const MONGO_URI = process.env.MONGO_URI //: "mongodb://localhost:27017/auth-api";
 
 module.exports = {
   MONGO_URI,
